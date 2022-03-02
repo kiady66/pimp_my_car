5705c08323a9253b4fd1b08ce46c41f2962a789f
kiady66 <kiady.raveloson@heavn.app>
2022-03-02 19:41:36 +0100

    change to postgres

--- src/main/java/com/example/pimp_my_car/PimpMyCarApplication.java
+++ src/main/java/com/example/pimp_my_car/PimpMyCarApplication.java
@@ -1,13 +1,10 @@
 package com.example.pimp_my_car;
 
-import com.example.pimp_my_car.client.Client;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -19,6 +16,5 @@ public class PimpMyCarApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-
 	}
 }
